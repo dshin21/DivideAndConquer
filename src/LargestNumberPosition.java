03af5b7d4d9e7f1986dc498426ba592199fed059
Daniel Shin <danielshin234@gmail.com>
2018-10-08 23:03:28 -0700

    finished

---
+++ src/LargestNumberPosition.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class LargestNumberPosition {
+    public static void main( String[] args ) {
+        int[] inputArray = { 2, 5, 8, 3, 6, 9, 1, 6, 5 };
+        int position = findPosition( inputArray, 0, inputArray.length - 1 );
+
+        System.out.println( Arrays.toString( inputArray ) );
+        System.out.println( "Largest number in the array is: " + inputArray[ position ] + " at index: " + position );
+    }
+
+    private static int findPosition( int[] inputArray, int head, int tail ) {
+        int max;
+
+        if ( head == tail )
+            max = head;
+        else {
+            int headMax = findPosition( inputArray, head, ( head + tail ) / 2 );
+            int tailMax = findPosition( inputArray, ( ( head + tail ) / 2 ) + 1, tail );
+            if ( inputArray[ headMax ] > inputArray[ tailMax ] )
+                max = headMax;
+            else
+                max = tailMax;
+        }
+        return max;
+    }
+}
---
+++ src/MergeSort.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+
+public class MergeSort {
+    public static void main( String[] args ) {
+        ArrayList<Integer> A = new ArrayList<>();
+        A.add( 2 );
+        A.add( 5 );
+        A.add( 8 );
+        A.add( 3 );
+        A.add( 6 );
+        A.add( 9 );
+        A.add( 1 );
+        A.add( 6 );
+        A.add( 5 );
+
+        System.out.println( "Original A: " + A );
+        A = mergeSort( A );
+        System.out.println( "After merge sort: " + A );
+    }
+
+    private static ArrayList<Integer> mergeSort( ArrayList<Integer> A ) {
+        ArrayList<Integer> B = new ArrayList<>();
+        ArrayList<Integer> C = new ArrayList<>();
+        int mid = A.size() / 2;
+
+        if ( A.size() > 1 ) {
+            for ( int i = 0; i < mid; i++ )
+                B.add( A.get( i ) );
+            for ( int j = mid; j < A.size(); j++ )
+                C.add( A.get( j ) );
+
+            B = mergeSort( B );
+            C = mergeSort( C );
+            merge( B, C, A );
+        }
+        return A;
+    }
+
+    private static void merge( ArrayList<Integer> B, ArrayList<Integer> C, ArrayList<Integer> A ) {
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        ArrayList<Integer> temp;
+        int index;
+
+        while ( a < B.size() && b < C.size() ) {
+            if ( B.get( a ) <= C.get( b ) ) {
+                A.set( c, B.get( a ) );
+                a++;
+            } else {
+                A.set( c, C.get( b ) );
+                b++;
+            }
+            c++;
+        }
+
+        if ( a == B.size() ) {
+            temp = C;
+            index = b;
+        } else {
+            temp = B;
+            index = a;
+        }
+
+        for ( int i = index; i < temp.size(); i++ ) {
+            A.set( c, temp.get( i ) );
+            c++;
+        }
+    }
+}
